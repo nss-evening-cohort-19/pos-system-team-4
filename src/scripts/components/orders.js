a84842a9367466cdedff56b4570830d2dd9a2c8a
Adam Steel <asteel88@gmail.com>
2022-06-16 20:14:44 -0500

    order cards

---
+++ src/scripts/components/orders.js
@@ -0,0 +1,30 @@
+import clearDom from '../helpers/clearDom';
+import renderToDOM from '../helpers/renderToDom';
+
+const showOrders = (array) => {
+  clearDom();
+
+  let domString = '';
+  array.forEach(() => {
+    domString += `
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>`;
+  });
+  renderToDOM('#main', domString);
+};
+
+const emptyOrders = () => {
+  document.querySelector('#main').innerHTML = '<h1>No Items</h1>';
+};
+
+export {
+  showOrders,
+  emptyOrders,
+};
