29b1e77534f4283892009ac18874ecf7478e4cea
Patrick Burns <burns212@gmail.com>
2022-06-18 10:13:20 -0500

    put in some basic orders API calls

---
+++ src/api/foodItemsData.js
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+// import firebaseConfig from './apiKeys';
+
+// const dbUrl = firebaseConfig.databaseURL;
+
+// const getFoodItems = () => {
+//   axios.get etc.
+// };
---
+++ src/api/paymentTypesData.js
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+// import firebaseConfig from './apiKeys';
+
+// const dbUrl = firebaseConfig.databaseURL;
+
+// const getPaymentTypes = () => {
+//   axios.get etc.
+// };
