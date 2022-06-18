92a7b2000143b58c1f01c44ae8250d59e014284b
Micah Stotsky <Leowyatt09@gmail.com>
2022-06-18 10:38:26 -0500

    food item promises

---
+++ src/api/foodItemsData.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import firebaseConfig from './apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
+
+// GET ALL FOOD ITEMS 
+const getFood = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/foodItems.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+// CREATE FOOD ITEMS 
+const createFood = (foodObject) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/foodItems.json`, foodObject)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/foodItems/${response.data.name}.json`, payload)
+        .then(() => {
+            getFood(foodObject).then(resolve);
+        });
+    }).catch(reject);
+})
+// DELETE FOOD ITEMS
+const deleteFood = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/foodItems/${firebaseKey}.json`)
+    .then(() => {
+      getFood().then((foodArray) => resolve(foodArray));
+    })
+    .catch((error) => reject(error));
+});
+// UPDATE FOOD ITEMS
+const updateFood = () => (foodObject) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/foodItems/${foodObject.firebaseKey}.json`, foodObject)
+    .then(() => getFood().then(resolve))
+    .catch(reject);
+});
+
+export {
+  getFood,
+  createFood,
+  deleteFood,
+  updateFood
+}
