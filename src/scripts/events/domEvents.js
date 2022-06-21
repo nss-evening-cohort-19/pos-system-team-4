9953f0d42e4e39154f1721a5d0757940a22d05e2
Tatiana Pronoza <tpronoza1@gmail.com>
2022-06-21 16:47:02 -0500

    small changes and will need to be adjusted

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,6 +1,8 @@
 // import { getFoodItemsByOrderId } from '../../api/foodItemsData';
 import { getOrders } from '../../api/ordersData';
 import { showOrders } from '../components/orders';
+import { getFoodItemsByOrderId } from '../../api/foodItemsData';
+import orderDetails from '../components/pages/orderDetailsPage';
 
 const domEvents = () => {
   document.querySelector('#main').addEventListener('click', (e) => {
@@ -8,9 +10,8 @@ const domEvents = () => {
       getOrders().then((orderArray) => showOrders(orderArray));
     }
     if (e.target.id.includes('orderDetails')) {
-      // const [, firebaseKey] = e.target.id.split('--');
-      // getFoodItemsByOrderId(firebaseKey).then((itemArray) => showItems(itemArray));
-      //
+      const [, firebaseKey] = e.target.id.split('--');
+      getFoodItemsByOrderId(firebaseKey).then((itemArray) => orderDetails(itemArray));
       // 'showItems' is just a placeholder for whatever you call your function
       // you will need to uncomment this block
       // and uncomment the getFoodItemsByOrderId block
