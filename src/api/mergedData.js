bdf1c523bd41d372b5f0784c2727abe6a6a29124
Patrick Burns <burns212@gmail.com>
2022-06-23 21:07:57 -0500

    merging before commit

--- src/api/mergedData.js
+++ src/api/mergedData.js
@@ -1,6 +1,11 @@
 /* eslint-disable implicit-arrow-linebreak */
+import axios from 'axios';
+import firebaseConfig from './apiKeys';
 import { deleteFood } from './foodItemsData';
-import { deleteOrder, getOrderItems } from './ordersData';
+import { deleteOrder, getOrderItems, getOrder } from './ordersData';
+import { createRevenue } from './revenueData';
+
+const dbUrl = firebaseConfig.databaseURL;
 
 const deleteOrderItems = (orderID) => new Promise((resolve, reject) => {
   getOrderItems(orderID).then((itemsArray) => {
@@ -12,4 +17,28 @@ const deleteOrderItems = (orderID) => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 
-export default deleteOrderItems;
+const closeOrder = (firebaseKey) => new Promise((resolve, reject) => {
+  const closingPayload = { isClosed: true };
+  axios.patch(`${dbUrl}/orders/${firebaseKey}.json`, closingPayload)
+    .then(() => {
+      getOrder(firebaseKey)
+        .then((orderObj) => {
+          const revenueObj = {
+            callInOrder: orderObj.callInOrder,
+            dateTime: orderObj.dateTime,
+            firebaseKey: '',
+            orderID: orderObj.orderID,
+            paymentID: orderObj.paymentID,
+            tip: orderObj.tip,
+            total: orderObj.total,
+          };
+          createRevenue(revenueObj);
+        });
+    })
+    .catch((error) => reject(error));
+});
+
+export {
+  deleteOrderItems,
+  closeOrder
+};
