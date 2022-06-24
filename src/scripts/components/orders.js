ed93a488870ba6673a38dc5c61d22d7227aacfd0
Patrick Burns <burns212@gmail.com>
2022-06-23 19:30:37 -0500

    allowed the form to accept existing objects, added event, did some styling

--- src/scripts/components/orders.js
+++ src/scripts/components/orders.js
@@ -4,20 +4,20 @@ const showOrders = (array) => {
   let domString = '<div id="viewOrdersDiv">';
   array.forEach((orders) => {
     domString += `
-    <div class="card" id="viewOrders" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${orders.orderName}</h5>
-    <hr><p class="card-text">${orders.isClosed === true ? 'Closed' : 'Open'}</p></hr>
-    <hr><p class="card-text">${orders.phone}</p></hr>
-    <hr><p class="card-text">${orders.email}</p></hr>
-    <hr><p class="card-text">${orders.callInOrder === true ? 'Call In Order' : 'Walk In Order'}</p></hr>
+  <div class="card orderCard" id="viewOrders">
+    <div class="card-body">
+      <h5 class="card-title">${orders.orderName}</h5>
+      <hr><p class="card-text">${orders.isClosed === true ? 'Closed' : 'Open'}</p></hr>
+      <hr><p class="card-text">${orders.phone}</p></hr>
+      <hr><p class="card-text">${orders.email}</p></hr>
+      <hr><p class="card-text">${orders.callInOrder === true ? 'Call In Order' : 'Walk In Order'}</p></hr>
     </div>
     <div class="card-footer">
-    <a href="#" class="card-link" id="orderDetails--${orders.firebaseKey}">Details</a>
-    <a href="#" class="link-warning" id="editOrder--${orders.firebaseKey}">Edit</a>
+      <a href="#" class="card-link" id="orderDetails--${orders.firebaseKey}">Details</a>
+      <a href="#" class="link-warning" id="editOrder--${orders.firebaseKey}">Edit</a>
     <div class="link-danger">${orders.isClosed === false ? `<a href="#" id="delete-order-btn--${orders.firebaseKey}" class="link-danger">Delete</a>` : ''}</div>
     </div>
-</div>`;
+  </div>`;
   });
   domString += '</div>';
   renderToDOM('#main', domString);
